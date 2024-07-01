5d44e87cdee8e9e303a69873acaf4dd83547ff06
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-01 17:52:00 +0200

    update

--- query.sql
+++ query.sql
@@ -1,25 +1,51 @@
--- Visualizzazione Base
+-- 1. Visualizzazione Base
 -- Scrivi una query per visualizzare tutte le informazioni di tutti i treni.
 -- 100
 
--- Selezione per Agente
+-- 2. Selezione per Agente
 -- Scrivi una query per elencare tutti i treni gestiti dall'agenzia 'Cremin LLC'.
 -- 1
 
--- Stazioni Particolari
+-- 3. Stazioni Particolari
 -- Scrivi una query per elencare i treni che partono da 'South Sean' e arrivano a 'Bergnaumstad'.
 -- 1
 
--- Codici Unici dei Treni
+-- 4. Codici Unici dei Treni
 -- Scrivi una query per selezionare i treni con il codice che inizia con 'train-9'.
 -- 13
 
--- Treni Puntuali
+-- 5. Treni Puntuali
 -- Scrivi una query per trovare tutti i treni che sono arrivati in tempo (in_time = 1).
 -- 100
 
--- Treni con Molti Vagoni
+-- 6. Treni con Molti Vagoni
 -- Scrivi una query per trovare i treni che hanno più di 15 vagoni.
 -- 33
 
+-- 7. Treni per Specifiche Stazioni di Partenza e Arrivo
+-- Scrivi una query per elencare i treni che partono da 'East Wendychester' e arrivano a 'Lake Dustyville'.
+-- 1
 
+-- 8. Treni Ordinati per Orario di Partenza
+-- Scrivi una query per elencare i treni ordinati per start_time in ordine crescente.
+-- 100
+
+-- 9. Treni con Numero Specifico di Carrozze
+-- Scrivi una query per trovare i treni che hanno esattamente 10 vagoni.
+-- 8
+
+-- 10. Filtro per Più Agenzie
+-- Scrivi una query per elencare i treni gestiti sia da 'Lang and Sons' che da 'Kling-Nienow'.
+-- 2
+
+-- 11. Treni Non Cancellati con Pochi Vagoni
+-- Scrivi una query per trovare i treni non cancellati (deleted = 0) con meno di 7 vagoni.
+-- 8
+
+-- BONUS
+
+-- 12. Ricerca per Fascia Oraria
+-- Scrivi una query per trovare i treni che partono tra le 22:00 e le 04:00.
+--
+-- vedi funzione HOUR()
+-- 37
