67163c5f9f406b3ff48f7f5a703b55d9d1d5b312
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-01 17:25:27 +0200

    update

--- query.sql
+++ query.sql
@@ -1,27 +1,25 @@
 -- Visualizzazione Base
 -- Scrivi una query per visualizzare tutte le informazioni di tutti i treni.
+-- 100
 
 -- Selezione per Agente
 -- Scrivi una query per elencare tutti i treni gestiti dall'agenzia 'Cremin LLC'.
-
--- Intervallo di Tempo di Viaggio
--- Scrivi una query per trovare i treni il cui tempo di viaggio (differenza tra start_time e end_time) supera le 72 ore.
+-- 1
 
 -- Stazioni Particolari
 -- Scrivi una query per elencare i treni che partono da 'South Sean' e arrivano a 'Bergnaumstad'.
+-- 1
 
 -- Codici Unici dei Treni
 -- Scrivi una query per selezionare i treni con il codice che inizia con 'train-9'.
+-- 13
 
 -- Treni Puntuali
 -- Scrivi una query per trovare tutti i treni che sono arrivati in tempo (in_time = 1).
+-- 100
 
 -- Treni con Molti Vagoni
 -- Scrivi una query per trovare i treni che hanno più di 15 vagoni.
+-- 33
 
--- Treni Cancellati
--- Scrivi una query per elencare i treni che sono stati cancellati (deleted = 1).
-
--- Ultimi Aggiornamenti
--- Scrivi una query per trovare i treni che sono stati aggiornati per l'ultima volta dopo il 15 febbraio 2024.
 
